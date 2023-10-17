c964d0f29fd5bfc4eebd9281e3096243a79fb2cd
kushwaharajan <148178976+kushwaharajan@users.noreply.github.com>
2023-10-17 10:42:21 +0530

    Add files via upload
    MyResume Committed

---
+++ index.js
@@ -0,0 +1,74 @@
+
+$('#colorpicker').on("change", function () {
+    var bg = $('#colorpicker').val();
+    $("body").css("background-color", bg);
+});
+
+$('#Navcolorpicker').on("change", function () {
+    var bgv = $('#Navcolorpicker').val();
+    $("#sideNav").css("background-color", bgv);
+});
+
+$('#txtcolorpicker').on("change", function () {
+    var txt = $('#txtcolorpicker').val();
+    $("body").css("color", txt);
+});
+
+$('#myDropdown').on("change", function () {
+    var font1 = $('#myDropdown').val();
+    $("body").css("font-family", font1);
+});
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
